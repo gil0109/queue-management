c90a5282c28b17b4d388459fb2ed7c668ca3afc2
gil0109 <karim.gillani@gov.bc.ca>
2020-11-15 15:30:24 -0800

    x

--- sonarqube.groovy
+++ sonarqube.groovy
@@ -65,7 +65,7 @@ podTemplate(
             SONAR_PROJECT_NAME = 'Queue Management'
             SONAR_PROJECT_KEY = 'queue-management'
             SONAR_PROJECT_BASE_DIR = '../'
-            SONAR_SOURCES = '.'
+            SONAR_SOURCES = './'
 
             SONARQUBE_PWD = sh (
                 script: 'oc set env dc/sonarqube --list | awk  -F  "=" \'/SONARQUBE_KEY/{print $2}\'',
