5867fc5fe6e480bd69d9bdccba276aeb2859dc4c
gil0109 <karim.gillani@gov.bc.ca>
2020-11-16 19:43:45 -0800

    Add OC

--- JenkinsfileOCP4.groovy
+++ JenkinsfileOCP4.groovy
@@ -374,8 +374,12 @@ podTemplate(
         parallel Zap_Fronend: {
             stage('ZAP Security Scan') {
                 sleep 60
+                sh (
+                    script: 'wget -c https://github.com/openshift/origin/releases/download/v3.11.0/openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit.tar.gz -O - | tar -xz openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit/oc && mv openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit/oc /var/lib/jenkins/'
+                )
+
                 ZAP_WITH_URL = sh (
-                    script: 'oc describe configmap jenkin-config | awk  -F  "=" \'/^zap_with_url_staff/{print $2}\'',
+                    script: '/var/lib/jenkins/oc describe configmap jenkin-config | awk  -F  "=" \'/^zap_with_url_staff/{print $2}\'',
                     returnStdout: true
                 ).trim()            
                 def retVal = sh (
@@ -403,8 +407,11 @@ podTemplate(
         }, Zap_Appointment: {
             stage('ZAP Security Scan') {
                 sleep 60
+                sh (
+                    script: 'wget -c https://github.com/openshift/origin/releases/download/v3.11.0/openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit.tar.gz -O - | tar -xz openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit/oc && mv openshift-origin-client-tools-v3.11.0-0cbc58b-linux-64bit/oc /var/lib/jenkins/'
+                )
                 ZAP_WITH_URL = sh (
-                    script: 'oc describe configmap jenkin-config | awk  -F  "=" \'/^zap_with_url/{print $2}\'',
+                    script: '/var/lib/jenkins/oc describe configmap jenkin-config | awk  -F  "=" \'/^zap_with_url/{print $2}\'',
                     returnStdout: true
                 ).trim()            
                 def retVal = sh (
