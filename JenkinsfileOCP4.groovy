61e5959cbb83afca699d2fc4da2df71df2b43df9
gil0109 <karim.gillani@gov.bc.ca>
2020-11-16 18:25:54 -0800

    Add Zap registry

--- JenkinsfileOCP4.groovy
+++ JenkinsfileOCP4.groovy
@@ -359,7 +359,7 @@ podTemplate(
     cloud: 'openshift', 
     containers: [ containerTemplate(
         name: 'jnlp',
-        image: 'jenkins-slave-zap',
+        image: 'image-registry.openshift-image-registry.svc:5000/5c0dde/jenkins-slave-zap',
         resourceRequestCpu: '500m',
         resourceLimitCpu: '1000m',
         resourceRequestMemory: '3Gi',
