9430fff22ca69164784306e251bb13fe059b8173
gil0109 <karim.gillani@gov.bc.ca>
2020-11-16 22:29:22 -0800

    test

--- JenkinsfileOCP4.groovy
+++ JenkinsfileOCP4.groovy
@@ -369,6 +369,7 @@ podTemplate(label: 'zap', name: 'zap', serviceAccount: 'jenkins', cloud: 'opensh
        stage('Scan Web Application') {
          dir('/zap') {
                 def retVal = sh returnStatus: true, script: '/zap/zap-baseline.py -r baseline.html -t http://platform-dev.pathfinder.gov.bc.ca/'
+                publishHTML([allowMissing: false, alwaysLinkToLastBuild: false, keepAll: true, reportDir: '/zap/wrk', reportFiles: 'baseline.html', reportName: 'ZAP Baseline Scan', reportTitles: 'ZAP Baseline Scan'])
                 echo "Return value is: ${retVal}"
          }
        }
